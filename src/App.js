4e3c62686232729954b19786f496b65987c35a6e
William Gorges <william.gorges.ext@sascar.com.br>
2020-04-06 14:09:31 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Jumbotron,
+  Container,
+  Row,
+  Col,
+  CardColumns,
+  Card,
+  ListGroup,
+  ListGroupItem,
+} from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class List extends React.Component {
+  state = {
+    characters: [],
+    loading: true,
+    error: null,
+  };
+
+  async componentDidMount() {
+    try {
+      this.setState({ loading: true });
+      await fetch("https://www.breakingbadapi.com/api/characters")
+        .then((res) => res.json())
+        .then((res) => {
+          this.setState({
+            characters: res,
+          });
+        });
+      this.setState({ loading: false });
+    } catch (error) {
+      this.setState({ loading: false, error });
+    }
+  }
+
+  render() {
+    if (this.state.loading) return "Carregando...";
+    if (this.state.error)
+      return "Algo deu Errado, por favor tente novamente mais tarde.";
+    return (
+      <div>
+        <Container>
+          <Col>
+            <Row>
+              <Jumbotron>
+                <h1>Lista de Personagens de Breaking Bad</h1>
+                <p>Teste realizado por William Franz.</p>
+              </Jumbotron>
+            </Row>
+          </Col>
+          <CardColumns>
+            {console.log(this.state.characters[0])}
+            {this.state.characters.map((item) => (
+              <Card key={item.char_id.toString()}>
+                <Card.Img
+                  variant="top"
+                  src={item.img}
+                  style={{ width: "18rem" }}
+                />
+                <Card.Body>
+                  <Card.Title>{item.name}</Card.Title>
+                  <ListGroup className="list-group-flush">
+                    <ListGroupItem>Nickname: {item.nickname}</ListGroupItem>
+                    <ListGroupItem>
+                      Data de Nascimento: {item.birthday}
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      Interpretado(a) por: {item.portrayed}
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      Ocupações:
+                      {item.occupation.map(
+                        (occupation) => " " + occupation + "; "
+                      )}
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      Aparições (Temp.):
+                      {item.appearance.map(
+                        (appearance) => " " + appearance + "; "
+                      )}
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      Aparições em Better Caul Saul (Temp.):
+                      {item.better_call_saul_appearance.map(
+                        (appearance) => " " + appearance + "; "
+                      )}
+                    </ListGroupItem>
+                  </ListGroup>
+                </Card.Body>
+              </Card>
+            ))}
+          </CardColumns>
+        </Container>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default List;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import waitUntil from "async-wait-until";
+import Adapter from "enzyme-adapter-react-16";
+import { shallow, configure } from "enzyme";
 
-test('renders learn react link', () => {
+beforeAll(() => {
+  configure({ adapter: new Adapter() });
+});
+
+it("Testa loading", async () => {
+  const wrapper = shallow(<App />);
+  const instance = wrapper.instance();
+  await waitUntil(() => instance.state.characters !== []);
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = getByText(/carregando/i);
+  expect(element).toBeTruthy();
 });
